ba2b3cad0b66e8e409d3defa9f83ebea8ed526ff
MeowMixer <meowmixer@meowmix-ThinkPad-X220.(none)>
2013-08-24 18:04:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cheatsheetz::Application.config.secret_key_base = '251598bb05b9230cbd09291d21a72856db55eb6341d288a9e7b83dbc2519fad08ee983daa4b111addc2257a752d449fe0a1023ee58a1b7a96f694280bb7e6770'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cheatsheetz::Application.config.session_store :cookie_store, key: '_cheatsheetz_session'
